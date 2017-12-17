90c164b04ead39217f5dd0b0a85993e1641ccb22
Carlos Solsona <carlos.solsona@gmail.com>
2017-12-17 19:24:12 +0100

    Update the app title

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Pomodoro Timer </h1>
         </header>
         <p className="App-intro">
           <Timer seconds={10 * 60} onTick={this.onTick} />
